9657c8918c9e5542f3ef288863c282d38928f85f
Max Pavlov <max@radacode.com>
2021-12-11 22:44:03 +0200

    Update last_green_commit.sh

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -4,6 +4,7 @@ pushd ${DIRNAME}
 header="PRIVATE-TOKEN: ${PRIVATE_TOKEN}"
 urlToCall="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
 echo "URL to call is: ${urlToCall}"
+echo "Heads is: ${header}
 curl --header "${header}" -s "${urlToCall}" -o tmp
 echo "pipelines statuses are:"
 head tmp
