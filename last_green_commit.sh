b1fdf01d417028a85294115985da7dd2e35f8aeb
Max Pavlov <max@radacode.com>
2021-12-11 22:32:46 +0200

    Adding logging

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -2,8 +2,13 @@
 DIRNAME="$(dirname $(readlink -f "$0"))"
 pushd ${DIRNAME}
 header="PRIVATE-TOKEN: ${PRIVATE_TOKEN}"
-url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
-curl --header "${header}"  -s "${url}" -o tmp
+urlToCall = "${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
+echo "URL to call is: ${urlToCall}"
+curl --header "${header}" -s "${urlToCall}" -o tmp
+echo "pipelines statuses are:"
+head tmp
+echo "jq.filter is:"
+echo jq.filter
 jq -r -f jq.filter < tmp > .LAST_GREEN_COMMIT
 echo "Last green commit is"
 cat .LAST_GREEN_COMMIT
