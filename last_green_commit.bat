38c88e4bf008c291c6abbc4771146b36f0be3d1b
Max Pavlov <max@radacode.com>
2021-12-14 23:50:13 +0200

    Update last_green_commit.bat

--- last_green_commit.bat
+++ last_green_commit.bat
@@ -3,8 +3,8 @@ setlocal
 pushd %~dp0
 set header="PRIVATE_TOKEN: %PRIVATE_TOKEN%"
 set header_delayed="PRIVATE_TOKEN: !PRIVATE_TOKEN!"
-echo header is '%header$'
-echo header_delayed is '%header_delayed$'
+echo header is '%header%'
+echo header_delayed is '%header_delayed%'
 set url="%CI_SERVER_URL%/api/v4/projects/%CI_PROJECT_ID%/pipelines?status=success&ref=%CI_COMMIT_REF_NAME%"
 curl -H %header% -s %url% | jq -r -f jq.filter > .LAST_GREEN_COMMIT
 set /P commit=<.LAST_GREEN_COMMIT
