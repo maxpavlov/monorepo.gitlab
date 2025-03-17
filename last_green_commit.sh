ae2f2e229823466345c698855fb9a6c24cf06086
Max Pavlov <max@radacode.com>
2025-03-17 22:55:11 +0200

    Update last_green_commit.sh

--- last_green_commit.sh
+++ last_green_commit.sh
@@ -5,7 +5,6 @@ header="PRIVATE-TOKEN: ${PROJECT_ACCESS_TOKEN}"
 urlToCall="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/pipelines?status=success&ref=${CI_COMMIT_REF_NAME}"
 echo "URL to call is: ${urlToCall}"
 curl --header "${header}" -s "${urlToCall}" -o tmp
-cat tmp
 jq -r -f jq.filter < tmp > .LAST_GREEN_COMMIT
 echo "Last green commit is"
 cat .LAST_GREEN_COMMIT
